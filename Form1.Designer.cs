5a16e543fee5e37266aa714c0e6f121ab3ef60c4
Joshua Cambal <joshuacambal01@gmail.com>
2023-04-14 15:39:40 +0800

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace Classic_Student_Grade_Application
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtmath = new System.Windows.Forms.TextBox();
+            this.txtenglish = new System.Windows.Forms.TextBox();
+            this.txtscience = new System.Windows.Forms.TextBox();
+            this.txthistory = new System.Windows.Forms.TextBox();
+            this.txtsports = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btngenerate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtname
+            // 
+            this.txtname.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtname.Location = new System.Drawing.Point(67, 21);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(226, 22);
+            this.txtname.TabIndex = 0;
+            // 
+            // txtmath
+            // 
+            this.txtmath.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtmath.Location = new System.Drawing.Point(84, 84);
+            this.txtmath.Name = "txtmath";
+            this.txtmath.Size = new System.Drawing.Size(64, 22);
+            this.txtmath.TabIndex = 0;
+            this.txtmath.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtenglish
+            // 
+            this.txtenglish.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtenglish.Location = new System.Drawing.Point(84, 112);
+            this.txtenglish.Name = "txtenglish";
+            this.txtenglish.Size = new System.Drawing.Size(64, 22);
+            this.txtenglish.TabIndex = 0;
+            this.txtenglish.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtscience
+            // 
+            this.txtscience.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtscience.Location = new System.Drawing.Point(84, 140);
+            this.txtscience.Name = "txtscience";
+            this.txtscience.Size = new System.Drawing.Size(64, 22);
+            this.txtscience.TabIndex = 0;
+            this.txtscience.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txthistory
+            // 
+            this.txthistory.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txthistory.Location = new System.Drawing.Point(84, 168);
+            this.txthistory.Name = "txthistory";
+            this.txthistory.Size = new System.Drawing.Size(64, 22);
+            this.txthistory.TabIndex = 0;
+            this.txthistory.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtsports
+            // 
+            this.txtsports.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtsports.Location = new System.Drawing.Point(84, 196);
+            this.txtsports.Name = "txtsports";
+            this.txtsports.Size = new System.Drawing.Size(64, 22);
+            this.txtsports.TabIndex = 0;
+            this.txtsports.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label9
+            // 
+            this.label9.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(26, 87);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(40, 18);
+            this.label9.TabIndex = 1;
+            this.label9.Text = "MATH";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(5, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "ENGLISH";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(7, 140);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "SCIENCE";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(4, 168);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 18);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "HISTORY";
+            // 
+            // label4
+            // 
+            this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(8, 196);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 18);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "SPORTS";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(14, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 18);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "NAME";
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(87, 62);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(56, 18);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Grades";
+            // 
+            // btngenerate
+            // 
+            this.btngenerate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btngenerate.Font = new System.Drawing.Font("Consolas", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btngenerate.Location = new System.Drawing.Point(162, 62);
+            this.btngenerate.Name = "btngenerate";
+            this.btngenerate.Size = new System.Drawing.Size(144, 156);
+            this.btngenerate.TabIndex = 2;
+            this.btngenerate.Text = "GENERATE AVERAGE";
+            this.btngenerate.UseVisualStyleBackColor = true;
+            this.btngenerate.Click += new System.EventHandler(this.btngenerate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(318, 227);
+            this.Controls.Add(this.btngenerate);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.txtsports);
+            this.Controls.Add(this.txthistory);
+            this.Controls.Add(this.txtscience);
+            this.Controls.Add(this.txtenglish);
+            this.Controls.Add(this.txtmath);
+            this.Controls.Add(this.txtname);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Grade Application";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtmath;
+        private System.Windows.Forms.TextBox txtenglish;
+        private System.Windows.Forms.TextBox txtscience;
+        private System.Windows.Forms.TextBox txthistory;
+        private System.Windows.Forms.TextBox txtsports;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btngenerate;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Classic_Student_Grade_Application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btngenerate_Click(object sender, EventArgs e)
+        {
+            if (txtenglish.Text.Equals("") || txthistory.Text.Equals("") || txtmath.Text.Equals("") || txtname.Text.Equals("") || txtscience.Text.Equals("") || txtsports.Text.Equals(""))
+            {
+                MessageBox.Show("Field is blank", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                try
+                {
+                    double english = Convert.ToDouble(txtenglish.Text);
+                    double math = Convert.ToDouble(txtmath.Text);
+                    double science = Convert.ToDouble(txtscience.Text);
+                    double sports = Convert.ToDouble(txtsports.Text);
+                    double history = Convert.ToDouble(txthistory.Text);
+
+                    double average = (english + math + science + sports + history) / 5;
+
+                    if (average >= 75 && average <= 100)
+                    {
+                        MessageBox.Show("     The Student passed.\nThe general average of " + txtname.Text + " is " + average, "School", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                        txtenglish.Text = null;
+                        txtsports.Text = null;
+                        txthistory.Text = null;
+                        txtmath.Text = null;
+                        txtscience.Text = null;
+                        txtname.Text = null;
+                    }
+                    else if (average <= 74 && average >= 51)
+                    {
+                        MessageBox.Show("     The Student failed.\nThe general average of " + txtname.Text + " is " + average, "School", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        txtenglish.Text = null;
+                        txtsports.Text = null;
+                        txthistory.Text = null;
+                        txtmath.Text = null;
+                        txtscience.Text = null;
+                        txtname.Text = null;
+                    }
+                    else if (average > 100)
+                    {
+                        MessageBox.Show("     The Student Grade exceed limit.", "School", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                        txtenglish.Text = null;
+                        txtsports.Text = null;
+                        txthistory.Text = null;
+                        txtmath.Text = null;
+                        txtscience.Text = null;
+                        txtname.Text = null;
+                    }
+                    else
+                    {
+                        MessageBox.Show("     The Student dropped.\nThe general average of " + txtname.Text + " is " + average, "School", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        txtenglish.Text = null;
+                        txtsports.Text = null;
+                        txthistory.Text = null;
+                        txtmath.Text = null;
+                        txtscience.Text = null;
+                        txtname.Text = null;
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Something Wrong", "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            
+        }
+    }
+}
